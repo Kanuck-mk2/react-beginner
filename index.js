3f958f4f8ce620bd13f82214e35db0fe1fa958b2
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2023-12-08 15:57:23 -0700

    update check

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
-import React from "react"
-import ReactDOM from "react-dom"
-
-
-function Page() {
-    return (
-  <>
+function Header() {
+  return (
     <header>
-            <nav>
-               <img src="./react-logo.png" width="40px" />
-            </nav>
-            </header>
-       
-        <h1>Why I want to learn React</h1>
-        <ol>
-            <li>Its a skill</li>
-            <li>Further my understanding in programming</li>
-            <li>take pride that this skill is "self taught"</li>
-            <li>Hopefully lead to a promising career</li>
-            <li>Have some use for it in the changing, uncertain future of this world</li>
-            
-        </ol>
-        <footer>
-        <small> © 2021 Parenteau development. All rights reserved.</small>
-        </footer>
-    </>
-    
-   )
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
 }
 
+function Footer() {
+  return (
+    <footer>
+      <small> © 2021 Parenteau development. All rights reserved.</small>
+    </footer>
+  );
+}
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+function MainContent() {
+  return (
+    <>
+      <h1>Why I want to learn React</h1>
+      <ol>
+        <li>Its a skill</li>
+        <li>Further my understanding in programming</li>
+        <li>take pride that this skill is "self taught"</li>
+        <li>Hopefully lead to a promising career</li>
+        <li>
+          Have some use for it in the changing, uncertain future of this world
+        </li>
+      </ol>
+    </>
+  );
+}
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
